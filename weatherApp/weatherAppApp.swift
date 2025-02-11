68201810029765adadacdbded1523947e0efe46a
Berkay-Aykose <berkay_ayk@hotmail.com>
2025-02-11 21:06:06 +0100

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Berkay Veysel Ayköse on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
